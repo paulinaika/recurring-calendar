1e2b75374aa1e430c9cf76249da90d6ec2817e1a
Paulina  Sudjatmiko <paulina@Paulinas-MacBook-Air.local>
2017-06-30 11:52:17 +1000

    devise added

--- Gemfile
+++ Gemfile
@@ -5,8 +5,15 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#Devise gem
+gem 'devise'
+
+#Calendar
 gem "simple_calendar"
+
+#Calendar -recurring events
 gem "recurring_select", github:"RundownApp/recurring_select"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629015401) do
+ActiveRecord::Schema.define(version: 20170630014701) do
 
   create_table "advisor_exceptions", force: :cascade do |t|
     t.integer "advisor_id"
@@ -27,6 +27,23 @@ ActiveRecord::Schema.define(version: 20170629015401) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.datetime "end_time"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
